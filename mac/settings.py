4ce6bdd794178c3bf2cc5df980f04eb359f0ee35
rijo7 <rijov929@gmail.com>
2025-06-20 18:02:13 +0530

    Fix ALLOWED_HOSTS for Render deployment

--- mac/settings.py
+++ mac/settings.py
@@ -33,7 +33,7 @@ STRIPE_PUBLISHABLE_KEY = os.getenv('STRIPE_PUBLISHABLE_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://mac-1.onrender.com']
+ALLOWED_HOSTS = ['mac-1.onrender.com', 'localhost', '127.0.0.1']
 
 SECURE_SSL_REDIRECT = True
 
