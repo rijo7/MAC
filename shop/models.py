3c47ac3bc472111df71f01269805c75149c2f95d
rijo7 <rijov929@gmail.com>
2025-06-23 11:30:23 +0530

    Update: Enable HTTPS and Cloudinary image support

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class Product(models.Model):
@@ -10,7 +11,7 @@ class Product(models.Model):
     price = models.IntegerField(default=0)
     desc = models.CharField(max_length=300)
     pub_date = models.DateField()
-    image = models.ImageField(upload_to='shop/images', default="")
+    image = CloudinaryField('image')
 
     def __str__(self):
         return self.product_name
